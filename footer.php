6e6f0f708f9f5e1d8ac212face35baa059c2732a
Xristos9 <sonic9495@gmail.com>
2022-01-14 23:08:01 +0200

    troll

--- footer.php
+++ footer.php
@@ -1,7 +1,7 @@
 <footer class="p-5 bg-dark text-white text-center position-relative">
 	<div class="container">
 		<p class="lead">Copyright &copy; 2022 WEB Project</p>
-		<a href="#" class="position-absolute bottom-0 end-0 p-5">
+		<a href="https://www.youtube.com/watch?v=h3RNElMbdUY" target="_blank" class="position-absolute bottom-0 end-0 p-5">
 			<i class="bi bi-arrow-up-circle h1"></i>
 		</a>
 	</div>
