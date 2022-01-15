e6ea6ee4c89119118cf9b042d67e7b1003838b58
Xristos9 <sonic9495@gmail.com>
2022-01-15 19:17:01 +0200

    div

--- adminDeclare.php
+++ adminDeclare.php
@@ -42,14 +42,7 @@
 							</div>
 						</div>
 					</div>
-					<div class="col-md">
-						<div class="card bg-secondary text-light">
-							<div class="card-body text-center">
-								<h3 class="card-title mb-3">You visited these stores were there was a reported covid case:</h3>
-								<ul class="list-group" id="j"></ul>
-							</div>
-						</div>
-					</div>
+					
 				</div>
 			</div>
 		</section>
